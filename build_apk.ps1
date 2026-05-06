ae9c3d7a50f9f1482910b4db130b884eb90a5a4f
f-shake <autodotua@outlook.com>
2026-05-06 21:07:59 +0800

    成功编译.NET10的MAUI应用

--- build_apk.ps1
+++ build_apk.ps1
@@ -1,3 +1,3 @@
-﻿dotnet publish MapBoard.MAUI -f net9.0-android -r android-arm64 -c Release -p:AndroidKeyStore=true 
-Invoke-Item MapBoard.MAUI\bin\Release\net9.0-android\android-arm64\publish
+﻿dotnet publish MapBoard.MAUI -f net10.0-android -r android-arm64 -c Release -p:AndroidKeyStore=true 
+Invoke-Item MapBoard.MAUI\bin\Release\net10.0-android\android-arm64\publish
 pause
